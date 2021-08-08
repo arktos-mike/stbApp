5c9f50ad29a790899ae8ce8543d1e0f4da3dc4e5
arktos-mike <arktos.mike@gmail.com>
2021-08-08 14:23:56 +0300

    fix

--- src/router.js
+++ src/router.js
@@ -52,9 +52,6 @@ export class MainRouter extends React.Component {
 
         if (this.isElectron()) {
             window.ipcRenderer.send("appLoaded");
-            //i18next.init({
-            //    resources: require(langPath)
-            //});
             window.ipcRenderer.on('plcReplyMultiple', this.plcReplyMultipleListener);
             window.ipcRenderer.on('langChanged', (event, lang) => {
                 i18next.changeLanguage(lang, () => { });
